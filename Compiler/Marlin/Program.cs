5757f130f27f63a25eecca10f65c3eec5b7843a6
pitaya4 <im.crazy.dim0@gmail.com>
2021-06-01 10:21:10 +0300

    Cleanup
    Restart of the compiler project

--- Compiler/Marlin/Program.cs
+++ Compiler/Marlin/Program.cs
@@ -21,8 +21,8 @@ namespace Marlin
         public static bool DEBUG_MODE { get; private set; } = false;
         public static bool CREATE_FILE_TREE_GRAPHS { get; private set; } = false;
         public static string SOURCE_DIR { get; private set; } = "";
-        public static string START_CLASS { get; private set; } = "Main";
-        public static string MODULE_NAME { get; private set; } = "UnnamedModule";
+        public static string OUTPUT_DIR { get; private set; } = "";
+        public static string START_CLASS { get; private set; } = "";
         public static string TARGET { get; private set; } = "LLVM";
 
         public static void Main(string[] args)
@@ -52,9 +52,14 @@ namespace Marlin
                 Console.WriteLine("No source directory! Run \"marlin --help\" for instructions.");
                 Environment.Exit(0);
             }
-            if (!options.HasOption("--moduleName", true))
+            if (!options.HasOption("--output", true))
             {
-                Console.WriteLine("No module name! Run \"marlin --help\" for instructions.");
+                Console.WriteLine("No output directory! Run \"marlin --help\" for instructions.");
+                Environment.Exit(0);
+            }
+            if (!options.HasOption("--name", true))
+            {
+                Console.WriteLine("No name! Run \"marlin --help\" for instructions.");
                 Environment.Exit(0);
             }
 
@@ -62,29 +67,9 @@ namespace Marlin
             CREATE_FILE_TREE_GRAPHS = options.HasOption("--treeGraphs");
             SOURCE_DIR = options.GetOption("--src");
             START_CLASS = options.HasOption("--startClass", true) ? options.GetOption("--startClass") : START_CLASS;
-            MODULE_NAME = options.GetOption("--moduleName");
-            TARGET = options.HasOption("--target", true) ? options.GetOption("--target").ToUpper() : "LLVM";
-
-            if (TARGET != "CLI" && TARGET != "LLVM" && TARGET != "TEST")
-            {
-                Console.WriteLine("Invalid target! Run \"marlin --help\" for instructions.");
-                Environment.Exit(0);
-            }
 
             if (!SOURCE_DIR.EndsWith(Path.DirectorySeparatorChar) && !SOURCE_DIR.EndsWith(Path.AltDirectorySeparatorChar))
                 SOURCE_DIR += Path.DirectorySeparatorChar;
-
-            if (DEBUG_MODE)
-            {
-                Console.WriteLine("Started Marlin compiler");
-                Console.WriteLine("   Args:        " + string.Join(' ', args));
-                Console.WriteLine("   Debug:       true");
-                Console.WriteLine("   Source dir:  " + SOURCE_DIR);
-                Console.WriteLine("   Start class: " + START_CLASS);
-                Console.WriteLine("   Module:      " + MODULE_NAME);
-                Console.WriteLine("   Target:      " + TARGET);
-                Console.WriteLine();
-            }
         }
 
         private static void ShowHelp()
@@ -95,14 +80,13 @@ namespace Marlin
             Console.WriteLine();
             Console.WriteLine("Required options:");
             Console.WriteLine("  --src         The source directory.");
-            Console.WriteLine("  --moduleName  The name of the module. A module is essentially a project.");
+            Console.WriteLine("  --out         The directory to put the output file(s) in.");
+            Console.WriteLine("  --name        The name of the executable that the compiler produces.");
             Console.WriteLine();
             Console.WriteLine("Options:");
             Console.WriteLine("  --help        Shows this prompt.");
             Console.WriteLine("  --version     Displays version information.");
-            Console.WriteLine("  --startClass  The class to start from. It must have a main() function.");
             Console.WriteLine("  --debug       Debug mode while compiling - extra output.");
-            Console.WriteLine("  --target      Can be either LLVM or CLI. Defaults to LLVM.");
         }
 
         private static void ShowVersion(bool omitCopyright = false)
